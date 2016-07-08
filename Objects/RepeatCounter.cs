27f66cdb4564e332b72c98afc5e250024955f6d0
Charles Ewel <Charles.Ewel@gmail.com>
2016-07-08 09:12:25 -0700

    create project.lock.json and test for compilation errors

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,16 @@
+
+using Nancy;
+using System;
+using System.Collections.Generic;
+
+namespace RepeatCounter.Objects
+{
+  public class RepeatCounter
+  {
+    public int CountRepeats(string inputString, string inputWord)
+    {
+      // eventually your code will go here
+      return 0;
+    }
+  }
+}
