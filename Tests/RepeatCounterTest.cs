95551e69bd374716d7c36536003a50ab4627f45a
Charles Ewel <Charles.Ewel@gmail.com>
2016-07-08 09:06:51 -0700

    write tests. update readme such that sample inputs better reflect 'one behaviour at a time' philosophy

---
+++ Tests/RepeatCounterTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+namespace RepeatCounter.Objects
+{
+  public class RepearCounterTest
+  {
+    [Fact]
+    public void CountRepeats_TotalMismatch_0()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(0, newRepeatCounter.CountRepeats("the cat in the cathedral", "dog"));
+    }
+    [Fact]
+    public void CountRepeats_ExactMatch_1()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(1, newRepeatCounter.CountRepeats("cat", "cat"));
+    }
+    [Fact]
+    public void CountRepeats_AllMatches_2()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(2, newRepeatCounter.CountRepeats("the cat in the cathedral", "cat"));
+    }
+    [Fact]
+    public void CountRepeats_SpacedFullMatches_1()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(1, newRepeatCounter.CountRepeats("the scatter cat cathedral building", "cat"));
+    }
+    [Fact]
+    public void CountRepeats_PunctuatedFullMatches_2()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(2, newRepeatCounter.CountRepeats("the 'cat' cat.", "cat"));
+    }
+    [Fact]
+    public void CountRepeats_AllFullMatches_3()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.Equal(2, newRepeatCounter.CountRepeats("cat by the cat", "cat"));
+    }
+  }
+}
